8bf7b33a741d048d3285c83f1c0fa026729cc750
Sujan Sapkota <asmitmen@gmail.com>
2019-11-08 10:18:41 +0545

    initial

---
+++ CoffeeFlash/CoffeeFlash.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace CoffeeFlash
+{
+    public static class CoffeeFlash
+    {
+
+        public const string SUCCESS_INDEX = "success-message";
+        public const string ERROR_INDEX = "error-message";
+        public const string INFO_INDEX = "info-message";
+
+        public static void AddSuccessMessage(this Controller controller, string message)
+        {
+            var messageList = getMessageListByIndex(controller, SUCCESS_INDEX);
+            messageList.Add(message);
+            setMessageListByIndex(controller, messageList, SUCCESS_INDEX);
+        }
+
+        public static void AddErrorMessage(this Controller controller, string message)
+        {
+            var messageList = getMessageListByIndex(controller, ERROR_INDEX);
+            messageList.Add(message);
+            setMessageListByIndex(controller, messageList, ERROR_INDEX);
+        }
+        public static void AddInfoMessage(this Controller controller, string message)
+        {
+            var messageList = getMessageListByIndex(controller, INFO_INDEX);
+            messageList.Add(message);
+            setMessageListByIndex(controller, messageList, INFO_INDEX);
+        }
+
+        private static List<string> getMessageListByIndex(Controller c, string index)
+        {
+            return c.TempData[index] as List<string> ?? new List<string>();
+        }
+        private static void setMessageListByIndex(Controller controller, List<string> messageList, string index)
+        {
+            controller.TempData[index] = messageList;
+        }
+    }
+}
---
+++ CoffeeFlashExample/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoffeeFlashExample.Models;
+using CoffeeFlash;
+
+namespace CoffeeFlashExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Ask(string type)
+        {
+            switch (type)
+            {
+                case "success":
+                    this.AddSuccessMessage("This is a success message");
+                    break;
+                case "error":
+                    this.AddErrorMessage("This is an error message");
+                    break;
+                case "info":
+                    this.AddInfoMessage("This is an info message");
+                    break;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
